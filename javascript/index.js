30b58925b9608b231bb5a08fee3cbec2b3fa736a
lottiealdridge <lottiealdridge95@gmail.com>
2024-10-13 14:21:26 +0100

    added current location to dropdown js

--- javascript/index.js
+++ javascript/index.js
@@ -23,6 +23,9 @@ parisTimeElement.innerHTML =`${parisTime.format("h:mm:ss[<small>]A[<small>]")}`;
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current" ) {
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_"," ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
@@ -37,6 +40,7 @@ function updateCity(event) {
         </div>`
     
 }
+
 
 updateTime();
 setInterval(updateTime, 1000);
