8ab067a6c42a6e583fc84ea2b2b0a9b579971a95
lottiealdridge <lottiealdridge95@gmail.com>
2024-10-13 14:32:15 +0100

    added tokyo to home page

--- javascript/index.js
+++ javascript/index.js
@@ -19,6 +19,16 @@ let parisTime = moment().tz("Europe/Paris");
 parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
 parisTimeElement.innerHTML =`${parisTime.format("h:mm:ss[<small>]A[<small>]")}`;
 }
+
+let tokyoElement = document.querySelector("#tokyo");
+if (tokyoElement) {
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime = moment().tz("Asia/Tokyo");
+
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML =`${tokyoTime.format("h:mm:ss[<small>]A[<small>]")}`;
+}
 }
 
 function updateCity(event) {
